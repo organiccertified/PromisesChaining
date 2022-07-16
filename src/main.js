288e8ae7602834af7549bc02476d3d0dff6e72ce
Erick M <erick.michelr@gmail.com>
2022-07-16 10:01:07 -0500

    first tries

--- src/main.js
+++ src/main.js
@@ -5,21 +5,36 @@ function updateIfExists(id, body) {
   const url = `${BASE_URL}/constellations/${id}`;
   return axios
     .get(url)
-    
-    
-    .then(() => {
-      axios.put(url, body).then(({ data }) => {
-        console.log(data);
-      });
-    })
-  
-    
-    
+    .then(({ data }) => {
+      return data.find(( { data }) => data.id == id); 
+       })
+    .then((exists) => {
+      if (exists)
+      return axios
+        .put(url, body)
+        .then(({ data }) => console.log(data));
+      })
     .catch((error) => {
       console.log(error.message);
-    });
+    })
 }
 
 module.exports = {
   updateIfExists,
 };
+
+
+
+// REFACTOR: NO NEED TO NEST THENs
+// axios
+//   .get(constellationsUrl)
+//   .then(({ data }) => {
+//     return data.find(( { name }) => name === leo.name);
+//   })
+//   .then((exists) => {
+//     if (exists) throw `Constellation "${leo.name}" already exists.`;
+//     return axios
+//       .post(constellationsUrl, leo)
+//       .then(({ data }) => console.log(data));
+//   })
+//   .catch(console.log);
