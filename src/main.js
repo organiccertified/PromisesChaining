57b8ea7fe7200c5123998718feeaa311ef0816e7
Erick M <erick.michelr@gmail.com>
2022-07-16 10:37:09 -0500

    adding comments

--- src/main.js
+++ src/main.js
@@ -2,31 +2,33 @@ const axios = require("../utils/axios");
 const BASE_URL = "http://localhost:5000";
 
 function updateIfExists(id, body) {
-  const url = `${BASE_URL}/constellations/${id}`;
+  const url = `${BASE_URL}/constellations/${id}`; //looks for id -OK
   return axios
     .get(url)
     .then(({ data }) => {
-      return data;
+      return data;          //returns data - ok
       })
-    .then((exists) => {
+    .then((exists) => {                               //this fails
       if (exists) throw `Constallation with "${id}" already exists`;
       return axios
-        .put(url, body)
+        .put(url, body)                               // because 11 fails this fails
         .then(({ data }) => console.log(data));
       })
-    .catch(({message}) => {
+    .catch(({message}) => {         // if ln 11 fails why msg nto gets printed correctly?
       console.error(message);    
     })
 }
 
+// test data
 const leo = {
   name: "Columba",
   meaning: "Dove",
   starsWithPlanets: 3,
   quadrant: "SQ1"
 };
-
+//local test
 console.log(updateIfExists("xxx", leo))
+//finish test block
 
 module.exports = {
   updateIfExists,
